d101e49b1922febd939010a025ae191b4439255b
fardabara <farhad_mohammadali@yahoo.com>
2018-02-28 19:47:52 +0330

    961209 my shop 001

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,111 +1,96 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 import {CollectionPage} from "./collection/collection";
-
-
-export interface Entry {
-  name: string;
-  imageUrl: string;
-}
+import {HttpClient} from '@angular/common/http';
+import {tick} from '@angular/core/testing';
+import {noUndefined} from '@angular/compiler/src/util';
 
 export interface Type {
-  name_en: string;
-  name_fa: string;
-  entries: Entry[];
 }
-
-interface Brand {
-  imageUrl: string;
+export interface Entry {
 }
-
 @Component({
   selector: 'page-products',
   templateUrl: 'products.html',
 })
 export class ProductsPage {
-  entries: Entry[] = [{
-    name: 'ترند',
-    imageUrl: 'https://i.pinimg.com/736x/4d/f6/a3/4df6a38b3aa59db5e1132868ac57c1e7--fashion-me-indie-fashion.jpg',
-  }, {
-    name: 'کفش',
-    imageUrl: 'https://smhttp-ssl-39255.nexcesscdn.net/wp-content/uploads/2013/07/Sanders-Shoes.jpg',
-  }, {
-    name: 'لباس',
-    imageUrl: 'http://images.wisegeek.com/stack-of-colorful-wash-cloths.jpg',
-  }, {
-    name: 'لوازم جانبی',
-    imageUrl: 'https://cdn1.polaris.com/globalassets/pga/apparel/helmets--goggles/2867822.jpg?v=403d6039'
-  }];
-  secondEntries: Entry[] = [{
-    name: 'ترند دوم',
-    imageUrl: 'https://i.pinimg.com/736x/4d/f6/a3/4df6a38b3aa59db5e1132868ac57c1e7--fashion-me-indie-fashion.jpg',
-  }, {
-    name: 'کفش دیگر',
-    imageUrl: 'https://smhttp-ssl-39255.nexcesscdn.net/wp-content/uploads/2013/07/Sanders-Shoes.jpg',
-  }, {
-    name: 'لباس های متنوع',
-    imageUrl: 'http://images.wisegeek.com/stack-of-colorful-wash-cloths.jpg',
-  }, {
-    name: 'لوازم جانبی',
-    imageUrl: 'https://cdn1.polaris.com/globalassets/pga/apparel/helmets--goggles/2867822.jpg?v=403d6039'
-  }];
+
+  subMenu = false;
+  types = [];
+  placement = [];
+  typeElements = [];
+  subList = [];
+  selectTab: string;
+  columnSelected: any;
 
 
-  types: Type[] = [
-    {
-      name_en: 'men',
-      name_fa: 'مردانه',
-      entries: this.entries
-    }, {
-      name_en: 'women',
-      name_fa: 'زنانه',
-      entries: this.secondEntries
-    }, {
-      name_en: 'boys',
-      name_fa: 'پسرانه',
-      entries: this.entries
-    }, {
-      name_en: 'girls',
-      name_fa: 'دخترانه',
-      entries: this.secondEntries
-    }
-  ];
-
-  brands: Brand[] = [
-    {imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Logo_NIKE.svg/1200px-Logo_NIKE.svg.png'},
-    {imageUrl: 'http://www.freelogovectors.net/wp-content/uploads/2016/12/hurley-logo.png'},
-    {imageUrl: 'https://www.underconsideration.com/brandnew/archives/converse_logo.png'},
-    {imageUrl: 'https://s3.amazonaws.com/sneakerfreaker-cdn/image/2017/NIKELAB.jpg?mtime=20171016172748'}
-  ];
-  currentType: string = this.types[0].name_en;
-
-  // collectionPage: CollectionPage;
-
-  constructor(public navCtrl: NavController) {
+  constructor(private http: HttpClient, public navCtrl: NavController) {
 
   }
 
-  goToCollection(type: Type, entry: Entry) {
-    this.navCtrl.push(CollectionPage, {
-      type: type,
-      entry: entry
+  ionViewWillEnter() {
+    this.types = [];
+    this.http.get('assets/shop.json').subscribe(data => {
+      this.placement = data['placement'];
+      this.placement.forEach(item => {
+        if (item.component_name === 'menu' && item.variable_name === 'topMenu') {
+          this.types.push({name: item.info.text, href: item.info.href})
+        }
+      });
+      this.elementType(this.types[0]);
     });
   }
 
-  getSeparatedRowBrands(s = 3) {
-    let total = [];
-    let chunk = [];
-    let i;
-    for(i = 0; i < this.brands.length; i++) {
-      chunk.push(this.brands[i]);
-      if(i % s == s-1 && i != 0) {
-        total.push(chunk);
-        chunk = [];
+  elementType(type) {
+    console.log("@@@@@@@",type);
+    this.typeElements = [];
+    type = type.href.split('/')[1];
+    this.placement.filter(el => {
+      let section;
+      if (el.info.section) {
+        section = el.info.section.split('/')[0];
       }
-    }
-    if(i % s != s-1)
-      total.push(chunk);
-    return total;
+      if (el.variable_name === 'subMenu' && section === type && el.info.is_header && el.info.column === el.info.row) {
+        return el;
+      }
+    }).forEach(item => {
+      this.typeElements.push({text: item.info.text, imageUrl: item.imageUrl, column: item.info.column})
+    });
+    this.selectTab = type;
   }
+
+  showSubMenu(entry) {
+    this.columnSelected = entry.column;
+    this.subList = [];
+    this.placement.filter(el => el.info.section !== undefined).forEach(item => {
+      if (item.info.section.split('/')[0] === this.selectTab && !item.info.is_header && item.info.column === this.columnSelected) {
+        this.subList.push({text: item.info.text, href: item.info.href});
+      }
+    });
+    this.subMenu = !this.subMenu;
+  }
+
+  // goToCollection(type: Type, entry: Entry) {
+  //   this.navCtrl.push(CollectionPage, {
+  //     type: type,
+  //     entry: entry
+  //   });
+  // }
+  //
+  // getSeparatedRowBrands(s = 3) {
+  //   let total = [];
+  //   let chunk = [];
+  //   let i;
+  //   for (i = 0; i < this.brands.length; i++) {
+  //     chunk.push(this.brands[i]);
+  //     if (i % s == s - 1 && i != 0) {
+  //       total.push(chunk);
+  //       chunk = [];
+  //     }
+  //   }
+  //   if (i % s != s - 1)
+  //     total.push(chunk);
+  //   return total;
+  // }
 
 }
