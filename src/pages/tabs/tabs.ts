b21a7c9a3efde55663bd03e427069fa5de3a160e
305-2 <sareh@bentoak.systems>
2018-03-12 15:53:55 +0330

    commit

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -5,6 +5,7 @@ import { InboxPage } from '../inbox/inbox';
 import {ProfilePage} from '../profile/profile';
 import {MyShopPage} from '../my-shop/my-shop';
 import {FeedPage} from '../feed/feed';
+import {CartService} from '../../services/cart.service';
 
 @Component({
   templateUrl: 'tabs.html',
@@ -16,8 +17,17 @@ export class TabsPage {
   tabBagRoot = BagPage;
   tabInboxRoot = InboxPage;
   tabProfileRoot = ProfilePage;
+  cartNum = '12';
 
-  constructor() {
+  constructor( private cartService: CartService) {
+  }
+
+  ngOnInit() {
+    this.cartNum = this.cartNum.toLocaleString();
+    // this.cartService.getCartFromStorage().subscribe(data => {
+    //   console.log(data);
+    //   this.cartNum = data.length ? '7' : '6';
+    // });
 
   }
 }
--- src/services/cart.service.ts
+++ src/services/cart.service.ts
@@ -4,6 +4,7 @@ import {Storage} from '@ionic/storage';
 
 @Injectable()
 export class CartService {
+  cnt = 0;
 
   constructor(private httpService: HttpService,
               private storage: Storage) {
@@ -13,25 +14,34 @@ export class CartService {
     this.storage.get('user')
       .then(user => {
         if (!user) {
-          if (cb) cb("not logged in");
+          if (cb) cb('not logged in');
           return;
         }
-
         let customer_id = user['id'] || null;
         let data = {customer_id, product_instance_id, number};
         this.httpService.post(`order`, data).subscribe(
           data => {
             if (!(data.n > 0 || data.nModified > 0))
-              return cb("nothing is changed");
+              return cb('nothing is changed');
 
             if (cb) cb(null);
           }, err => {
-            console.log("error in adding orderline", err);
-            if(cb) cb(err);
+            console.log('error in adding orderline', err);
+            if (cb) cb(err);
           }
         );
       });
+  }
 
+  // getCartItems() {
+  //   if (!this.authService.isLoggedIn.getValue()) {
+  //     this.getCartFromStorage();
+  //   }
 
+  // Get order-line details or all order details
+// }
+
+  getCartFromStorage() {
+    return JSON.parse(localStorage.getItem('cart')) === null ? [] : JSON.parse(localStorage.getItem('cart'));
   }
 }
