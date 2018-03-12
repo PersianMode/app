a69ea120f06b050c82cf40fb8c43cffe1f9e7ed7
fardabara <farhad_mohammadali@yahoo.com>
2018-03-12 19:14:32 +0330

    961221 added bag

--- src/pages/bag/bag.ts
+++ src/pages/bag/bag.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {LoadingController, NavController} from 'ionic-angular';
 
 interface Product {
   //for now, only name and cost
   name: string;
   cost: number;
+  product_color_id: string
+  thumbnail: string,
 }
 
 @Component({
@@ -12,14 +14,41 @@ interface Product {
   templateUrl: 'bag.html',
 })
 export class BagPage {
-  products: Product[] = [];
+  products: Product[] = [{
+    name: 'محصول ۱',
+    cost: 1000,
+    product_color_id: '5a9cf71a68b68c2897d19253',
+    thumbnail: 'assets/product-pic/13.jpg'
+
+  }, {
+    name: 'محصول ۱',
+    cost: 1500,
+    product_color_id: '5a9cf71a68b68c2897d1924f',
+    thumbnail: 'assets/product-pic/12.jpg'
+  }, {
+    name: 'محصول ۲',
+    cost: 2000,
+    product_color_id: '5a9cf71a68b68c2897d19251',
+    thumbnail: 'assets/product-pic/11.jpg'
+
+  }, {
+    name: 'محصول ۳',
+    cost: 3000,
+    product_color_id: '5a9cf71a68b68c2897d19252',
+    thumbnail: 'assets/product-pic/10.jpg'
+  }];
   isPromoCodeShown: Boolean = false;
   shippingCost: number = 0;
   estimatedTax: number = 0;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
 
   }
+
+  ngOnInit() {
+  }
+
+
 
   onClickedOnPromoCode() {
     this.isPromoCodeShown = !this.isPromoCodeShown;
@@ -27,7 +56,7 @@ export class BagPage {
 
   getSubtotalCost() {
     let cost = 0;
-    for(let p in this.products) {
+    for (let p in this.products) {
       cost += this.products[p].cost;
     }
     return cost;
@@ -37,6 +66,16 @@ export class BagPage {
     let cost = this.getSubtotalCost();
     cost += this.shippingCost + this.estimatedTax;
     return cost;
+  }
+
+  removeThisProduct(product) {
+    let loading = this.loadingCtrl.create({
+      duration: 2000,
+    });
+    setTimeout(() => {
+      this.products = this.products.filter(el => el.product_color_id !== product.product_color_id)
+    }, 1000);
+    loading.present();
   }
 
 }
