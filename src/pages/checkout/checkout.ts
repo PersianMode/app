d312795eb02f0c7922e4b935b957a3c41aa6df50
imans777 <iman.s_sani@yahoo.com>
2018-04-03 16:58:09 +0430

    complete checkout main page

--- src/pages/checkout/checkout.ts
+++ src/pages/checkout/checkout.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {NavParams} from "ionic-angular";
+import {priceFormatter} from "../../shared/lib/priceFormatter";
 
 @Component({
   selector: 'page-checkout',
@@ -9,9 +10,31 @@ export class CheckoutPage {
 
   finalTotal;
   headerData;
+  isPlaceOrderDisable;
 
   constructor(private navParams: NavParams) {
     this.finalTotal = this.navParams.get('finalTotal') || 0;
     this.headerData = this.navParams.get('headerData') || {};
+    this.isPlaceOrderDisable = true;
+  }
+
+  formatPrice(p) {
+    return priceFormatter(p);
+  }
+
+  popAddress() {
+
+  }
+
+  popPayment() {
+
+  }
+
+  popSummary() {
+
+  }
+
+  placeOrder() {
+
   }
 }
